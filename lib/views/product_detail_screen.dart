bd3520b8d68b91568ad50827d47e30e935aab143
Augusto Veiga <augustoveiga@MacBook-Pro-de-Augusto.local>
2020-05-17 21:47:50 -0300

    removed provider example

--- lib/views/product_detail_screen.dart
+++ lib/views/product_detail_screen.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:shop/providers/counter_provider.dart';
 import '../models/product.dart';
-import '../providers/counter_provider.dart';
 
-class ProductDetailScreen extends StatefulWidget {
-  @override
-  _ProductDetailScreenState createState() => _ProductDetailScreenState();
-}
+class ProductDetailScreen extends StatelessWidget {
 
-class _ProductDetailScreenState extends State<ProductDetailScreen> {
   @override
   Widget build(BuildContext context) {
     final Product product =
@@ -16,21 +10,6 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text(product.title),
-      ),
-      body: Column(
-        children: <Widget>[
-          Text(CounterProvider.of(context).counterState.value.toString()),
-          RaisedButton(
-            child: Text('+'),
-            onPressed: () {
-              setState(() {
-                //CounterProvider.of(context).counterState.inc();
-                context.dependOnInheritedWidgetOfExactType<CounterProvider>().counterState.inc();
-              });
-              print(CounterProvider.of(context).counterState.value);
-            },
-          ),
-        ],
       ),
     );
   }
