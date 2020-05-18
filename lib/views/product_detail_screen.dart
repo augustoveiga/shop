6eec565447e3e5d3294287569b6a86da5e64b7a3
Augusto Veiga <augustoveiga@MacBook-Pro-de-Augusto.local>
2020-05-17 21:43:00 -0300

    testing provider (examples)

--- lib/views/product_detail_screen.dart
+++ lib/views/product_detail_screen.dart
@@ -1,13 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:shop/providers/counter_provider.dart';
 import '../models/product.dart';
+import '../providers/counter_provider.dart';
 
-class ProductDetailScreen extends StatelessWidget {
+class ProductDetailScreen extends StatefulWidget {
+  @override
+  _ProductDetailScreenState createState() => _ProductDetailScreenState();
+}
+
+class _ProductDetailScreenState extends State<ProductDetailScreen> {
   @override
   Widget build(BuildContext context) {
-    final Product product = ModalRoute.of(context).settings.arguments as Product;
+    final Product product =
+        ModalRoute.of(context).settings.arguments as Product;
     return Scaffold(
       appBar: AppBar(
         title: Text(product.title),
+      ),
+      body: Column(
+        children: <Widget>[
+          Text(CounterProvider.of(context).counterState.value.toString()),
+          RaisedButton(
+            child: Text('+'),
+            onPressed: () {
+              setState(() {
+                //CounterProvider.of(context).counterState.inc();
+                context.dependOnInheritedWidgetOfExactType<CounterProvider>().counterState.inc();
+              });
+              print(CounterProvider.of(context).counterState.value);
+            },
+          ),
+        ],
       ),
     );
   }
