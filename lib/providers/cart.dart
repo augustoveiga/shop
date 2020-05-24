6a605f0815aa36740370d37b98ca9278a9027eac
Augusto Veiga <augustoveiga@MacBook-Pro-de-Augusto.local>
2020-05-23 22:23:59 -0300

    cart + cartitem -> #218

---
+++ lib/providers/cart.dart
@@ -0,0 +1,51 @@
+import 'dart:math';
+
+import 'package:flutter/foundation.dart';
+import './product.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final int qunatity;
+  final double price;
+
+  CartItem(
+      {@required this.id,
+      @required this.title,
+      @required this.price,
+      @required this.qunatity});
+}
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items;
+
+  Map<String, CartItem> get item {
+    return {..._items};
+  }
+
+  void addItem(Product product) {
+    if (_items.containsKey(product.id)) {
+      _items.update(
+        product.id,
+        (existingItem) {
+          return CartItem(
+            id: existingItem.id,
+            title: existingItem.title,
+            price: existingItem.price + 1,
+            qunatity: existingItem.qunatity,
+          );
+        },
+      );
+    } else {
+      _items.putIfAbsent(
+        product.id,
+        () => CartItem(
+          id: Random().nextDouble().toString(),
+          title: product.title,
+          qunatity: 1,
+          price: product.price
+        ),
+      );
+    }
+  }
+}
