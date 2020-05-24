e9a972efbf3f06299026e7f80194788b842d5ad1
Augusto Veiga <augustoveiga@MacBook-Pro-de-Augusto.local>
2020-05-24 04:10:35 -0300

    product detail  #225

--- lib/views/product_detail_screen.dart
+++ lib/views/product_detail_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import '../providers/product.dart';
 
 class ProductDetailScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     final Product product =
@@ -11,6 +10,36 @@ class ProductDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(product.title),
       ),
+      body: SingleChildScrollView(
+          child: Column(
+        children: <Widget>[
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(
+              product.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            'R\$ ${product.price}',
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 20,
+            ),
+          ),
+          SizedBox(height: 10),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            width: double.infinity,
+            child: Text(
+              product.description,
+              textAlign: TextAlign.center,
+            ),
+          )
+        ],
+      )),
     );
   }
 }
